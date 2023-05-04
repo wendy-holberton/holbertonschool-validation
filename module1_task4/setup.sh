ef338d8e317094a7c6535f7522ee7ab4ee73bdf2
wendy-holberton <113806658+wendy-holberton@users.noreply.github.com>
2023-05-04 22:31:11 +1000

    hugo

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # docker build -t ubtun .
-apt-get update && apt-get install -y make curl
+apt-get update && apt-get install -y hugo make curl
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_0.111.3_linux-arm64.deb > hugo.deb
 dpkg --install ./hugo.deb
 make build
