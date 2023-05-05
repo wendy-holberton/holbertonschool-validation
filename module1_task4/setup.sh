48967ca6fc0ec36dd7cdcce1f5403d6c30564bd9
wendy-holberton <113806658+wendy-holberton@users.noreply.github.com>
2023-05-05 15:38:31 +1000

    remove dist

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # docker build -t ubtun .
-apt-get update && apt-get install -y hugo make curl
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_0.111.3_linux-arm64.deb > hugo.deb
-dpkg --install ./hugo.deb
+apt-get update && apt-get install -y make curl
+curl -LO https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb 
+dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
 make build
