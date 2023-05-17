e7308adcedc937d0552337102f02fab843544395
wendy-holberton <113806658+wendy-holberton@users.noreply.github.com>
2023-05-17 21:51:19 +1000

    updated hugo

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # docker build -t ubtun .
 apt-get update && apt-get install -y make curl git
-curl -LO https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_linux-arm64.deb 
-dpkg -i hugo_extended_0.111.3_linux-arm64.deb
+curl -LO https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_linux-amd64.deb 
+dpkg -i hugo_extended_0.111.3_linux-amd64.deb
 apt install -y golang-go 
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b /usr/local/bin
