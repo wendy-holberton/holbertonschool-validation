8ff24f1fc2e5cd1a32c09746055cc5ecf69ddb0d
wendy-holberton <113806658+wendy-holberton@users.noreply.github.com>
2023-05-20 13:10:02 +1000

    markdownlint

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # docker build -t ubtun .
-apt-get update && apt-get install -y make curl git zip
+apt-get update && apt-get install -y make curl git zip jq
 
 # Github machine
 curl -LO https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_linux-amd64.deb 
@@ -12,4 +12,5 @@ dpkg -i hugo_extended_0.111.3_linux-amd64.deb
 apt install -y golang-go 
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b /usr/local/bin
 
-sudo npm install -g markdownlint-cli
+npm install -g markdownlint-cli
+npm install -g markdown-link-check
