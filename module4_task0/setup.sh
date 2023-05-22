97be78f974a884f28996750687042d500a7e160b
wendy-holberton <113806658+wendy-holberton@users.noreply.github.com>
2023-05-22 21:38:14 +1000

    setup

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -9,8 +9,8 @@ dpkg -i hugo_extended_0.111.3_linux-amd64.deb
 # curl -LO https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_linux-arm64.deb 
 # dpkg -i hugo_extended_0.111.3_linux-arm64.deb
 
-apt install -y golang-go 
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b /usr/local/bin
+# apt install -y golang-go 
+# curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b /usr/local/bin
 
 npm install -g markdownlint-cli
 npm install -g markdown-link-check
