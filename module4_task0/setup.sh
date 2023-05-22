e5cccdfa4e33f5ed33bd5207986429c540ac80c1
wendy-holberton <113806658+wendy-holberton@users.noreply.github.com>
2023-05-22 21:22:38 +1000

    dcoker container

---
+++ module4_task0/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# docker build -t ubtun .
+apt-get update && apt-get install -y make curl git zip jq
+
+# Github machine
+curl -LO https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_linux-amd64.deb 
+dpkg -i hugo_extended_0.111.3_linux-amd64.deb
+# Wendy machine
+# curl -LO https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_linux-arm64.deb 
+# dpkg -i hugo_extended_0.111.3_linux-arm64.deb
+
+apt install -y golang-go 
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b /usr/local/bin
+
+npm install -g markdownlint-cli
+npm install -g markdown-link-check
