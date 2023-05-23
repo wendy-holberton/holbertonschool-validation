415d77ff1d630b80e602c0d768fc7cdb9a8de09b
wendy-holberton <113806658+wendy-holberton@users.noreply.github.com>
2023-05-23 21:53:21 +1000

    install nvm

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -12,5 +12,12 @@ dpkg -i hugo_extended_0.111.3_linux-amd64.deb
 # apt install -y golang-go 
 # curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b /usr/local/bin
 
+# Setup nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 14
+
 npm install -g markdownlint-cli
 npm install -g markdown-link-check
